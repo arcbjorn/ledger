7dacb7b885bc562ab0f91401297a01203b2089e4
arcbjorn <arky.happ@gmail.com>
2025-10-04 03:23:50 -0300

    feat(routes): register delete account route

--- src/handlers/routes.ts
+++ src/handlers/routes.ts
@@ -1,7 +1,12 @@
 import { router } from '@handlers/router.ts';
-import { handleCreateAccount, handleGetAccount } from '@handlers/accounts.ts';
+import {
+  handleCreateAccount,
+  handleGetAccount,
+  handleDeleteAccount,
+} from '@handlers/accounts.ts';
 import { handleCreateTransaction } from '@handlers/transactions.ts';
 
 router.post('/accounts', handleCreateAccount);
 router.get('/accounts/:id', handleGetAccount);
+router.delete('/accounts/:id', handleDeleteAccount);
 router.post('/transactions', handleCreateTransaction);
